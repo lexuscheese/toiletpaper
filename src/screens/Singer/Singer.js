ccb7466c34b7577f3015a83e61c8e87a039c58c3
chrisyeung0628 <chrisyeung0628@gmail.com>
2018-02-17 23:16:02 +0800

    update Fetch API on singer page

--- src/screens/Singer/Singer.js
+++ src/screens/Singer/Singer.js
@@ -1,20 +1,53 @@
 'use strict';
 
 import React from 'react';
-import {View,
-    Text,StyleSheet,
+import {
+    View,
+    Text, StyleSheet,
 } from 'react-native';
+import ApiUtils from "../../utils/apiUtils";
 
 
 export default class singer extends React.Component<{}> {
     constructor(props) {
         super(props);
-        this.state={
+        this.state = {
+            date: [],
         }
     }
+
+    componentDidMount() {
+        fetch("http://www.19chord.com/php/get/getSinger.php", {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                    gender: this.props.navigation.state.params.gender,
+                }
+            ).then(ApiUtils.checkStatus).then((response) => {
+                const statusCode = response.status;
+                const data = response.json();
+                return Promise.all([statusCode, data]).then(res => ({
+                    statusCode: res[0],
+                    data: res[1]
+                }));
+            }).then((res) => {
+                const {statusCode, data} = res;
+                console.log("statusCode", statusCode);
+                console.log("data", data);
+                console.warn("data:" +data+ " and statusCode: "+data);
+            }).catch(error => {
+                console.error(error);
+                return {name: "network error", description: ""}
+            })
+        });
+    }
+
     render() {
         return (
-            <View style = {styles.container}>
+            <View style={styles.container}>
                 <Text>
                     {this.props.navigation.state.params.gender}
                 </Text>
@@ -22,14 +55,16 @@ export default class singer extends React.Component<{}> {
         );
     }
 };
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems:'center',
-        justifyContent:'center',
-    },
 
-});
+const
+    styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+
+    });
 
 
 
